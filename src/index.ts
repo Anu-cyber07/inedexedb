63b6f95d032389915e67b33b18785ce0573c0e33
Anu-cyber07 <75072999+Anu-cyber07@users.noreply.github.com>
2021-09-29 16:47:30 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-import { get, set } from 'idb-keyval';
-
-set('incidents', [{x: 1}, {y: 2}]);
-set('name',"anuprabha");
-
-get('incidents').then(console.log)
-get('name').then(console.log)
+import { get,set} from 'idb-keyval';
+var CryptoJS = require("react-native-crypto-js");
+var data = [{ id: 1, name: 'Anu' }, { id: 2, name: 'Abi' }]
+var ciphertext = CryptoJS.AES.encrypt(JSON.stringify(data), 'my-secret-key@123').toString();
+//console.log('Encrypt Data -')
+//console.log(ciphertext);
+var bytes = CryptoJS.AES.decrypt(ciphertext, 'my-secret-key@123');
+var decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+//console.log('decrypted Data -')
+//console.log(decryptedData);
+set('encrypt data',ciphertext );
+set('decrypt data',decryptedData);
+get('encrypt data').then(console.log)
+get('decrypt data').then(console.log)
